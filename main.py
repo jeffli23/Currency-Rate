397f8369fada1d87d87dd156a5ef7ffe7cfba815
Jeff Li <51106814+jeffli23@users.noreply.github.com>
2022-11-14 14:10:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_currency(in_currency, out_currency):
+    url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
+    content = requests.get(url).text
+    soup = BeautifulSoup(content,'html.parser')
+    currency = soup.find("span",class_="ccOutputRslt").get_text()
+    rate = float(currency[:-4])
+    
+    return rate
+
+current_rate=get_currency('USD','CNY')
+print(current_rate)
